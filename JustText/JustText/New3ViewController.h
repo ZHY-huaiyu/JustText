1f7a303418c798cf430a72a44f4602f8d4994c65
ZHY-huaiyu <925452290@qq.com>
2016-06-07 18:01:14 +0800

    new3

---
+++ JustText/JustText/New3ViewController.h
@@ -0,0 +1,13 @@
+//
+//  New3ViewController.h
+//  JustText
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface New3ViewController : UIViewController
+
+@end
---
+++ JustText/JustText/New3ViewController.m
@@ -0,0 +1,13 @@
+//
+//  New3ViewController.m
+//  JustText
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import "New3ViewController.h"
+
+@implementation New3ViewController
+
+@end
