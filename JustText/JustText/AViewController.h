341e040815d66e499ef516c1fc0d4192b2a5ef98
zln1764413771 <1764413771@qq.com>
2016-06-07 20:02:45 +0800

    A

---
+++ JustText/JustText/AViewController.h
@@ -0,0 +1,13 @@
+//
+//  AViewController.h
+//  JustText
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AViewController : UIViewController
+
+@end
