b8ce1373d88cb461fae4f24f5b45cf496fadeeb3
ZHY-huaiyu <925452290@qq.com>
2016-06-07 20:03:32 +0800

    no message

---
+++ JustText/JustText/BViewController.m
@@ -0,0 +1,13 @@
+//
+//  BViewController.m
+//  JustText
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import "BViewController.h"
+
+@implementation BViewController
+
+@end
