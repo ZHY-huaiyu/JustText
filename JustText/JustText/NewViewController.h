ae6c71aedc946fa36ae591674c7d92ed3d4e0dda
ZHY-huaiyu <925452290@qq.com>
2016-06-07 18:00:13 +0800

    view

---
+++ JustText/JustText/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  JustText
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
---
+++ JustText/JustText/NewViewController.m
@@ -0,0 +1,13 @@
+//
+//  NewViewController.m
+//  JustText
+//
+//  Created by lanou3g on 16/6/7.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import "NewViewController.h"
+
+@implementation NewViewController
+
+@end
